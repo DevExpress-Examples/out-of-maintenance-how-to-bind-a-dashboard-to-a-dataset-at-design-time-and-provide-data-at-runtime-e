86f8d1b951d6e8cf1543ba913ceb075c805d43df
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Dashboard_DataLoading/Dashboard1.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports Dashboard_DataLoading.nwindDataSetTableAdapters
+
+Namespace Dashboard_DataLoading
+	Partial Public Class Dashboard1
+		Inherits DevExpress.DashboardCommon.Dashboard
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+        Private Sub Dashboard1_DashboardLoading(ByVal sender As Object, ByVal e As EventArgs) _
+            Handles MyBase.DashboardLoading
+            Dim categories As New SalesPersonTableAdapter()
+            categories.Fill(Me.nwindDataSet1.SalesPerson)
+        End Sub
+	End Class
+End Namespace
